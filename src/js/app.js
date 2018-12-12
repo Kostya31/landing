d81e84fbcacd439529c2c6255781014340cdb95b
Kostetskyy Volodymyr <Kostetskyy.c.s@gmail.com>
2018-12-12 20:22:09 +0200

    mobile fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,14 @@
-window.$ = window.jQuery = require('jquery');
-window.Popper = require('popper.js').default;
+import $ from 'jquery';
+
+window.jQuery = $;
+window.$ = $;
+
+// require('bootstrap/js/src/util');
+// require('bootstrap/js/src/modal');
+
 
 import slick from 'slick-carousel';
 
-// import 'bootstrap/js/dist/util';
-// import 'bootstrap/js/dist/modal';
 
 let app = {
     questionSlider: function () {
@@ -13,14 +17,11 @@ let app = {
             sliderImg.each(function () {
                 let prevArrow = $(this).next().find('.slick-prev');
                 let nextArrow = $(this).next().find('.slick-next');
-
-                console.log($(this));
                 $(this).slick({
                     slidesToShow: 1,
                     appendDots: $(this).parent().find('.slider-dots'),
                     dotsClass: 'dots-items',
-                    customPaging: function(slider, i) {
-                        console.log(i);
+                    customPaging: function (slider, i) {
                         return '<span class="dot"></span>';
                     },
                     arrows: true,
@@ -31,32 +32,54 @@ let app = {
                     dots: true,
                     useTransform: false,
                 });
-
             })
-
         }
     },
 
     mobileNav: function () {
-      let hamburger = document.querySelector('.hamburger');
+        let hamburger = document.querySelector('.hamburger');
 
-      hamburger.addEventListener('click', toggleButton)
+        hamburger.addEventListener('click', toggleButton)
 
     },
 
+    switchLang: function () {
+        let switchBtn = document.querySelector('.switch-lang');
+        switchBtn.addEventListener('click', function (e) {
+            console.log('ee');
+            e.currentTarget.classList.toggle('open');
+        })
+    },
     tabs: function () {
         setTaggedTabActive();
+    },
+
+    modal: function () {
+        let openBtn = document.querySelector('.js-open-modal');
+        let closeBtn = document.querySelector('.js-close-modal');
+
+        openBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            openModal();
+        });
+
+        closeBtn.addEventListener('click', function (e) {
+            e.preventDefault();
+            closeModal(e);
+        })
     },
 
     init: function () {
         app.questionSlider();
         app.tabs();
+        app.switchLang();
+        // app.modal();
         app.mobileNav();
     }
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-   app.init();
+document.addEventListener('DOMContentLoaded', function () {
+    app.init();
 });
 
 let setTaggedTabActive = () => {
@@ -69,18 +92,17 @@ let setTaggedTabActive = () => {
         elem.addEventListener('click', function (e) {
             e.preventDefault();
             e.stopPropagation();
-            console.log('sds');
             let _this = e.currentTarget;
             let tabId = _this.getAttribute('data-tab');
 
-            for (let i of tabLinks){
+            for (let i of tabLinks) {
                 i.classList.remove('active');
             }
             _this.classList.add('active');
 
             for (let item of tabContent) {
                 item.classList.remove('active');
-                if( item.getAttribute('id') === tabId){
+                if (item.getAttribute('id') === tabId) {
                     item.classList.add('active')
                 }
             }
@@ -88,8 +110,23 @@ let setTaggedTabActive = () => {
     }
 };
 
+
+// function openModal() {
+//     document.querySelector('.modal').classList.add('show');
+//     document.querySelector('body').classList.add('modal-open');
+// }
+//
+// function closeModal(e) {
+//
+//     console.log(e);
+//     document.querySelector('.modal').classList.remove('show');
+//     document.querySelector('body').classList.remove('modal-open');
+// }
+
 function toggleButton() {
     document.querySelector('.hamburger').classList.toggle('is-active');
     document.querySelector('.header-nav').classList.toggle('open');
+    document.querySelector('html').classList.toggle('no-scroll');
 }
+
 
